fc5edd2ff498d0a25d672fa2f5e7940cb4f9a731
Sarthak Paswan <sarthak.kol54@gmail.com>
2024-08-12 10:56:40 +0530

    Updated Keyboard.js
    Added physical keyboard input for the user

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,10 +1,22 @@
-// src/Keyboard.js
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Keyboard.css';
 
 const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 const Keyboard = ({ onGuess, guessedLetters }) => {
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const key = event.key.toLowerCase(); 
+      if(letters.includes(key) && !guessedLetters.includes(key)){
+        onGuess(key);
+      }
+    };
+    window.addEventListener('keydown', handleKeydown);
+    return () => {
+      window.removeEventListener('keydown', handleKeydown); 
+    };
+  }, [guessedLetters, onGuess]);
+
   return (
     <div className="keyboard">
       {letters.map((letter) => (

868fd64d8a20891c48d539522c26fe970f047abb
Sarthak Paswan <sarthak.kol54@gmail.com>
2024-09-11 23:14:38 +0530

    Integrated ESLint and refactored code
    Integrated ESLint in our pipeline including react and code complexity plugins. Refactored the codebase as per the code quality issues raised by eslint.
    Updated keyboard.js, WordDisplay.js, App.js.

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Hangman.css';
 
 const Hangman = ({ wrongGuesses }) => {
@@ -29,6 +30,10 @@ const Hangman = ({ wrongGuesses }) => {
       </svg>
     </div>
   );
+};
+
+Hangman.propTypes = {
+  wrongGuesses: PropTypes.number.isRequired,
 };
 
 export default Hangman;
